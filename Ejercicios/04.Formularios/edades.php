30a162f4900e82997796b7bd30cd411388c3376e
anawuenoo <anabuenoo9916@gmail.com>
2024-10-08 14:26:42 +0200

    dia 08 t

---
+++ Ejercicios/04.Formularios/edades.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edades</title>
+</head>
+<body>
+    <!--
+    CREAR UN FORMULARIO QUE RECIBA DOS VALORES: EL NOMBREE Y LA EDAD DE UNA PERSONA
+
+    Si la persona tiene:
+
+        <18 años, se mostrará "X ES MENOR DE EDAD" (X es el nombre)
+        >= 18 && < 65, se mostrará "X ES MAYOR DE EDAD"
+        >= 65, se mostrará "X SE HA JUBILADO"
+        Hacer la lógica con la estructura MATCH
+    -->
+</body>
+</html>
