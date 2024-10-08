d04a73866a4cbbd2b173db2d0e1d7719485291e6
anawuenoo <anabuenoo9916@gmail.com>
2024-10-08 14:20:00 +0200

    dia 08

--- Ejercicios/04.Formularios/ejemplo.php
+++ Ejercicios/04.Formularios/ejemplo.php
@@ -18,8 +18,8 @@
     <form action="" method="post">
         <input type="text" name="mensaje">
         <input type="submit" value="Enviar">
-        <input type="text" name="numero">
-        <input type="submit" value="Envia">
+        <input type="text" name="veces">
+        
 
 
     </form>
@@ -31,7 +31,11 @@
          * una peticion  a través del método POST
          */
         $mensaje = $_POST ["mensaje"];
-        echo "<h1>$mensaje</h1>";
+        $veces = $_POST ["veces"];
+        for($i=0;$i<$veces;$i++){
+            echo "<h1>$mensaje</h1>";
+        }
+        
 
         /**
          * Modificar el formulario anterior para que se pueda introducir un número.
@@ -39,12 +43,9 @@
          * El mensaje se mostrará tantas veces como indique el número.
          */
 
-         if($_SERVER["REQUEST_METHOD"] == "POST"){
+        
 
-        $mensaje = $_POST ["numero"];
-        echo "<h1>$mensaje</h1>";
-
-         }
+         
 
     }
     
