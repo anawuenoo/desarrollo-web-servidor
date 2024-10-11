c0aa58d20d2d169ea1565304219cae95592cf2f1
anawuenoo <anabuenoo9916@gmail.com>
2024-10-11 11:18:56 +0200

    ejercicios lunes parte2

---
+++ Ejercicios/Ejercicios-Lunes/Ejercicio3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 3</title>
+    <?php
+      error_reporting( E_ALL );
+        ini_set( "display_errors", 1 );
+    ?>
+</head>
+<body>
+<?php
+    /**
+     * 
+     *Realiza un formulario que reciba dos números y devuelva todos los números 
+     *primos dentro de ese rango (incluidos los extremos).
+     * 
+     */
+    ?>
+    
+</body>
+</html>
